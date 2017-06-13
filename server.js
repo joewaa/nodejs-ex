75adfca854fd6f8be0f50cffb9e9a5dc082a97d2
joewaa <joewaa@users.noreply.github.com>
2017-06-13 08:54:27 +0800

    Create server.js

--- server.js
+++ server.js
@@ -4,12 +4,10 @@ var express = require('express'),
     app     = express(),
     eps     = require('ejs'),
     morgan  = require('morgan');
-    
-Object.assign=require('object-assign')
 
+Object.assign=require('object-assign')
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
-
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
@@ -57,6 +55,9 @@ var initDb = function(callback) {
     console.log('Connected to MongoDB at: %s', mongoURL);
   });
 };
+
+var routes = require('./api/routes/routes');
+routes(app);
 
 app.get('/', function (req, res) {
   // try to initialize the db on every request if it's not already
