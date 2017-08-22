2e3a80c4b31057f32fbc813f5c5f86a2504498a2
joewaa <joewaa@users.noreply.github.com>
2017-08-22 12:09:25 +0800

    Update app_test.js

--- tests/app_test.js
+++ tests/app_test.js
@@ -18,6 +18,16 @@ describe('Basic routes tests', function() {
         })
 
     })
+    it('GET to /hello should return 200', function(done){
+        chai.request(reqServer)
+        .get('/hello')
+        .end(function(err, res) {
+            res.should.have.status(200);
+            done();
+        })
+
+    })
+
 
     it('GET to /pagecount should return 200', function(done){
         chai.request(reqServer)
